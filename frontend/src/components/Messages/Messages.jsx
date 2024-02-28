8b8aecae5355d3d8039c2ee00c0bbe524ea34c73
p00jitha <dasaripujitha9@gmail.com>
2024-02-28 19:42:06 +0530

    messages

--- frontend/src/components/Messages/Messages.jsx
+++ frontend/src/components/Messages/Messages.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../../Hooks/useGetMessages'
 const Messages = () => {
+	const {messages,loading} = useGetMessages()
+	console.log(messages)
   return (
     <div className='px-4 flex-1 overflow-auto'>
  			<Message />
