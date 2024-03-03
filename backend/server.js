17c5ba85a9e725a077ec034edb1818589b2be847
p00jitha <dasaripujitha9@gmail.com>
2024-03-03 17:12:38 +0530

    deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -8,10 +9,14 @@ import userRoutes from './routes/user_routes.js'
 
 import connectdatabase from './db/connect_db.js'
 
-import { app,server} from "./socket/socket.js";
+import { app, server } from "./socket/socket.js";
 
 
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve();
+
+
 dotenv.config()
 app.use(express.json())
 
@@ -19,13 +24,19 @@ app.use(express.json())
     res.send('hello world')
 })*/
 app.use(cookieParser());
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRouter)
-app.use("/api/users",userRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRouter)
+app.use("/api/users", userRoutes)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-	connectdatabase()
-	console.log(`Server Running on port ${PORT}`);
+    connectdatabase()
+    console.log(`Server Running on port ${PORT}`);
 });
 
 /*app.listen(PORT,()=>{
