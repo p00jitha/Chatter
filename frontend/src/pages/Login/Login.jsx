be59fb3cde63e5c6ac341ad5ede17a3a005d1fdb
DASARI PUJITHA <129841150+p00jitha@users.noreply.github.com>
2026-05-29 21:14:28 +0530

    Remove black text color from input field

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
             <label className='label p-2'>
               <span className='text-base label-text text-white'>Username</span>
             </label>
-            <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10 text-black' value={username} onChange={(e) => setUsername(e.target.value)} />
+            <input type='text' placeholder='Enter username' className='w-full input input-bordered h-10' value={username} onChange={(e) => setUsername(e.target.value)} />
           </div>
           <div>
             <label className='label'>
@@ -32,7 +32,7 @@ const Login = () => {
             <input
               type='password'
               placeholder='Enter Password'
-              className='w-full input input-bordered h-10 text-black'
+              className='w-full input input-bordered h-10'
               value={password}
               onChange={(e) => setPassword(e.target.value)}
             />
